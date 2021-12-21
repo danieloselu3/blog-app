9589f4cee115a2d0d08073de98fb36737e2772a9
danieloselu3 <doselu3@gmail.com>
2021-12-21 05:59:58 +0300

    added ulr route for the details page

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 # imports
 from django.urls import path
 
-from .views import BlogPageListView
+from .views import BlogPageListView, BlogPageDetailView
 
 app_name = 'blog'
 urlpatterns = [
     path('', BlogPageListView.as_view(), name='home'),
+    path('post/<int:pk>/', BlogPageDetailView.as_view(), name='post_detail'),
 ]
